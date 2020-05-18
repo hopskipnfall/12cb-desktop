5bae10ac8129dc102d17eb379e112e3f0861cab5
Jonn Spencer <jonn@jonnjonnjonn.com>
2020-05-19 00:17:43 +0900

    Kill express server when application quits.

--- main.js
+++ main.js
@@ -30,15 +30,6 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow);
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-});
-
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
@@ -47,15 +38,25 @@ app.on('activate', () => {
   }
 });
 
-app.on('window-all-closed', () => {
-  app.quit();
-});
-
 // Launch child process to run the local server.
 const {
   fork
 } = require('child_process');
-const ps = fork(path.join(__dirname, '/server/bin/www'));
+const expressServerProcess = fork(path.join(__dirname, '/server/bin/www'));
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    expressServerProcess.kill();
+    app.quit();
+  }
+});
+
+app.on('quit', () => {
+  expressServerProcess.kill();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
