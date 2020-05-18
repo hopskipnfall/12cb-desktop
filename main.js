8acbb2316097693cb079d5b2eb22c2a16cd30af0
Jonn Spencer <jonn@jonnjonnjonn.com>
2020-05-18 23:10:08 +0900

    some changes!

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-const { app, BrowserWindow } = require('electron')
+const {
+  app,
+  BrowserWindow
+} = require('electron');
+const path = require('path');
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
@@ -8,19 +12,19 @@ function createWindow () {
     webPreferences: {
       nodeIntegration: true
     }
-  })
+  });
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadFile(path.join(__dirname, '/electron-client/dist/electron-client/index.html'));
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  win.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -29,23 +33,25 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 app.on('window-all-closed', () => {
   app.quit();
 });
 
 // Launch child process to run the local server.
-const { fork } = require('child_process')
-const ps = fork(`${__dirname}/server/bin/www`)
+const {
+  fork
+} = require('child_process');
+const ps = fork(path.join(__dirname, '/server/bin/www'));
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
